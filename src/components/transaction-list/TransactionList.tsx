5dfdc3c14b14c6f02e764934e28b50efd7d093af
ndelvalle <nicolas.delvalle@gmail.com>
2020-06-16 21:34:10 -0300

    Implement ws connection

--- src/components/transaction-list/TransactionList.tsx
+++ src/components/transaction-list/TransactionList.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
 import styled from 'styled-components'
+import createWebSocketClient from '../../services/ws'
+
+const { close, onMessage } = createWebSocketClient()
 
 const Container = styled.div``
 
 const TransactionList = () => {
-  return <Container></Container>
+  const [transactions, setTransactions] = useState([]);
+
+    useEffect(() => {
+      onMessage(function (transaction : any) {
+        setTransactions((transactions) => ([...transactions, transaction] as any))
+      })
+    }, []);
+
+  return <Container>
+    <p>Transactions</p>
+
+    <ul>
+      {transactions.map((transaction, index) => {
+        return <li key={index}>{JSON.stringify(transaction, null, 2)}</li>
+      })}
+    </ul>
+
+  </Container>
 }
 
 export default TransactionList
---
+++ src/services/ws.ts
@@ -0,0 +1,72 @@
+interface Transaction {
+  hash: string;
+  size: number;
+  time: Date;
+  spent: number;
+}
+
+type Listener = ((transaction: Transaction) => void);
+
+function parseTransaction(data: any): Transaction {
+    const content = data.x
+    const spent = content
+      .out
+      .reduce((total: number, { value }: { value: number }) => total + value, 0);
+
+    const transaction : Transaction = {
+      hash: content.hash,
+      size: content.size,
+      time: new Date(content.time),
+      spent
+    }
+
+    return transaction;
+}
+
+function createWebSocketClient (): any {
+  const socket: WebSocket = new WebSocket('ws://localhost:8080/ws/', ["wamp"]);
+  const service: any = {};
+  const listeners: Listener[] = []
+
+  // TODO: Add a remove listener function
+
+  service.onMessage = function (listener: Listener) {
+    console.log('Add event listener')
+    listeners.push(listener)
+  }
+
+  service.close = function () {
+    socket.close()
+  }
+
+  socket.addEventListener('open', function (event) {
+    // Keep connection alive
+    setInterval(() => socket.send('Ping'), 1000 * 1)
+  });
+
+  socket.addEventListener('message', function (event: any) {
+    try {
+
+      console.log(event.data)
+
+      if (event.data === 'Ping') {
+        return
+      }
+
+      const data = JSON.parse(event.data)
+      const transaction: Transaction = parseTransaction(data);
+
+      listeners.forEach(listener => listener(transaction));
+    } catch (err) {
+      // TODO: Handle errors
+      console.error(err)
+    }
+  });
+
+  return service;
+}
+
+export default createWebSocketClient
+
+
+
